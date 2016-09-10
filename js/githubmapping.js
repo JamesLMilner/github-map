deda31ca029a6a803585590283283b622b8e162b
James <jamesmilner22@hotmail.com>
2016-09-10 16:07:08 +0100

    better banding

--- js/githubmapping.js
+++ js/githubmapping.js
@@ -103,11 +103,11 @@
               if (rate > maxRate) maxRate = rate;
               icon = icons[1];
 
-              if (rate >= 1.344 && rate < 1.679 ) icon = icons[4];
-              if (rate >= 1.008 && rate < 1.343 ) icon = icons[3];
-              if (rate >= 0.673 && rate < 1.007 ) icon = icons[2];
-              if (rate >= 0.337 && rate < 0.672 ) icon = icons[1];
-              if (rate >= 0.000 && rate < 0.336 ) icon = icons[0];
+              if (rate >= 1.344 && rate <= 1.679 ) icon = icons[4];
+              if (rate >= 1.008 && rate <= 1.343 ) icon = icons[3];
+              if (rate >= 0.673 && rate <= 1.007 ) icon = icons[2];
+              if (rate >= 0.337 && rate <= 0.672 ) icon = icons[1];
+              if (rate >= 0.000 && rate <= 0.336 ) icon = icons[0];
 
               return L.marker(latlng, {
                   icon: L.icon({
